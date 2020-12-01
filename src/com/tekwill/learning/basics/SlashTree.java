04ac5d8ddf2869eb298dc45f6fac785d909b5541
dimashpro <sergiu.proca@gmail.com>
2020-12-01 18:14:38 +0100

    add a program wich is drawing a tree from slashes

---
+++ src/com/tekwill/learning/basics/SlashTree.java
@@ -0,0 +1,7 @@
+package com.tekwill.learning.basics;
+
+public class SlashTree {
+    public static void main(String[] args){
+        System.out.print("  \\/\n \\\\//\n\\\\\\///\n///\\\\\\\n //\\\\\n  /\\");
+    }
+}
