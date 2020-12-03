c9b3e35880de91ccfc9fbf78b47c152c022a2bc6
dimashpro <sergiu.proca@gmail.com>
2020-12-03 21:13:20 +0100

    play with acces modifiers in basic package Software

--- src/md/modifiers/software/Employee.java
+++ src/md/modifiers/software/Employee.java
@@ -1,4 +1,30 @@
 package md.modifiers.software;
 
 public class Employee {
+    public String firstName;
+    protected String inheritedAccesFirstName;
+    private String limitedAccesFirstName;
+    public Employee employee;
+    public void doWork() {
+    }
+    private void doPrivateWork(){
+
+    }
+    protected void doProtectedWork(){
+
+    }
+    public void checkAccesModifiers(){
+        firstName = "Sergiu";           //+++
+        employee.firstName = "Sergiu";  //+++
+        doWork();                       //+++
+        employee.doWork();              //+++
+        inheritedAccesFirstName = "Sergiu"; //+++
+        employee.inheritedAccesFirstName = "Sergiu";  //+++
+        doProtectedWork();              //+++
+        employee.doProtectedWork();     //+++
+        limitedAccesFirstName = "Sergiu"; //+++
+        employee.limitedAccesFirstName = "Sergiu";   //+++
+        doPrivateWork();            //+++
+        employee.doPrivateWork();     //+++
+    }
 }
--- src/md/modifiers/software/Meeting.java
+++ src/md/modifiers/software/Meeting.java
@@ -1,4 +1,19 @@
 package md.modifiers.software;
 
 public class Meeting {
+    private Employee employee;
+    public void checkAccesModifiers(){
+       // firstName = "Sergiu";           //---
+        employee.firstName = "Sergiu";  //+++
+        // doWork();                       //---
+        employee.doWork();              //+++
+        //inheritedAccesFirstName = "Sergiu"; //---
+        employee.inheritedAccesFirstName = "Sergiu";  //+++
+        //doProtectedWork();              //---
+        employee.doProtectedWork();     //+++
+        //limitedAccesFirstName = "Sergiu"; //---
+        //employee.limitedAccesFirstName = "Sergiu";   //---
+       // doPrivateWork();            //---
+       // employee.doPrivateWork();     //---
+    }
 }
