0866836d1f6016219a136b778c387113c9e15aa7
dimashpro <sergiu.proca@gmail.com>
2020-12-13 19:16:53 +0100

    solved problem with output of constants

--- src/com/tekwill/learning/datatypes/Constants.java
+++ src/com/tekwill/learning/datatypes/Constants.java
@@ -1,30 +1,31 @@
 package com.tekwill.learning.datatypes;
 
 public class Constants {
-    int a;
-    int b = 1;
-    double archimedesConstantPi1 = 3.141592653;
-    double archimedesConstantPi2 = Math.PI;
-    double eulersNumberE1 = 2.7182818284;
-    double eulersNumberE2 = Math.exp(1);
-    double i;  // imaginary unit i^2 = -1
-    double pythagorasConstant1 = 1.4142135623;
-    double pythagorasConstant2 = Math.sqrt(2);
-    double goldenRatioPsi1 = 1.6180339887;
-    double goldenRatioPsi2 = (1 + Math.sqrt(5))/2;
+    final int A = 0;
+    final int B = 1;
+    final double ARCHIMEDE_CONSTANT_PI = 3.14159265358979323846264338327950288;
+    double archimedesConstantPi = Math.PI;
+    final double EULER_NUMBER_E = 2.71828182845904523536028747135266249;
+    double eulersNumberE = Math.exp(1);
+    final String IMAGINARY_UNIT_I = "i^2 = -1";  // imaginary unit i^2 = -1
+    final double PYTHAGORA_CONSTANT = 1.414213562373095048801688724209;
+    double pythagorasConstant = Math.sqrt(2);
+    final double GOLDEN_RATIO =  1.61803398874989484820;
+    double goldenRatioPsi = (1 + Math.sqrt(5))/2;
 
     public static void main(String[] args){
         Constants constants = new Constants();
-        System.out.println("a = "+constants.a);
-        System.out.println("b = "+constants.b);
-        System.out.println("pi = "+constants.archimedesConstantPi1);
-        System.out.println("pi = "+constants.archimedesConstantPi2);
-        System.out.println("e = "+constants.eulersNumberE1);
-        System.out.println("e = "+constants.eulersNumberE2);
-        System.out.println("square root of 2 = "+constants.pythagorasConstant1);
-        System.out.println("square root of 2 = "+constants.pythagorasConstant2);
-        System.out.println("golden ratio = "+constants.goldenRatioPsi1);
-        System.out.println("golden ratio = "+constants.goldenRatioPsi2);
+        System.out.println("a = "+constants.A);
+        System.out.println("b = "+constants.B);
+        System.out.println("pi = "+constants.ARCHIMEDE_CONSTANT_PI);
+        System.out.println("pi = "+constants.archimedesConstantPi);
+        System.out.println("e = "+constants.EULER_NUMBER_E);
+        System.out.println("e = "+constants.eulersNumberE);
+        System.out.println("square root of 2 = "+constants.PYTHAGORA_CONSTANT);
+        System.out.println("square root of 2 = "+constants.pythagorasConstant);
+        System.out.println("golden ratio = "+constants.GOLDEN_RATIO);
+        System.out.println("golden ratio = "+constants.goldenRatioPsi);
+        System.out.println("Unit imaginary number is "+constants.IMAGINARY_UNIT_I);
 
 
     }
