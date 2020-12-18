1f91dc17fdacbab8eba83c573340f96b147e14b1
dimashpro <sergiu.proca@gmail.com>
2020-12-18 16:11:49 +0100

    solved exercice with latitude in degrees

--- src/com/tekwill/learning/datatypes/operators/LatitudeDecimalFormat.java
+++ src/com/tekwill/learning/datatypes/operators/LatitudeDecimalFormat.java
@@ -4,14 +4,20 @@ import java.util.Scanner;
 
 public class LatitudeDecimalFormat {
     public  static void main(String[] args){
-        Scanner scanner = new Scanner();
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, please enter the latitude in degrees, minutes and seconds");
         System.out.println("First, enter the degrees");
-        int latitude = scanner.nextInt();
+        int degrees = scanner.nextInt();
         System.out.println("Next, enter the minutes of arc");
         int minutesOfArc = scanner.nextInt();
         System.out.println("Please, enter the seconds of the arc");
         int secondsOfArc = scanner.nextInt();
+        double minutesInDecimal = degrees * 100 / 60;
+        double secondsInDecimal = secondsOfArc * 100 / 60;
+        double degreesInDecimal = degrees + minutesInDecimal * 0.01 + secondsInDecimal * 0.0001;
+        System.out.println("Latitude is " + degreesInDecimal);
+
+
         
 
     }
