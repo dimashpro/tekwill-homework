6f0bdd8ada08939d157ef9b74283d064592419ae
dimashpro <sergiu.proca@gmail.com>
2020-12-20 18:43:22 +0100

    modified ConvertHeight class

--- src/com/tekwill/learning/datatypes/operators/ConvertHeight.java
+++ src/com/tekwill/learning/datatypes/operators/ConvertHeight.java
@@ -3,14 +3,13 @@ package com.tekwill.learning.datatypes.operators;
 import java.util.Scanner;
 
 public class ConvertHeight {
-    public static final double ARC_TO_MINUTE = 60.0;
-    public static final double ARC_TO_SECONDS = 3600.0;
+    public static final int INCHES_IN_ONE_FOOT = 12;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your height in integer inches");
         int heightInInches = scanner.nextInt();
-        int heightInFeet = heightInInches / 12;
-        int restFromConvert = heightInInches % 12;
+        int heightInFeet = heightInInches / INCHES_IN_ONE_FOOT;
+        int restFromConvert = heightInInches % INCHES_IN_ONE_FOOT;
         System.out.println("height is " + heightInFeet + " feets " + restFromConvert + " inches");
     }
 }
