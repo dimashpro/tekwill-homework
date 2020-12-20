878dff97ec83f370a77b789bc67ca51e4e770a04
dimashpro <sergiu.proca@gmail.com>
2020-12-20 18:40:09 +0100

    modified LatitudeDecimal class

--- src/com/tekwill/learning/datatypes/operators/ConvertHeight.java
+++ src/com/tekwill/learning/datatypes/operators/ConvertHeight.java
@@ -3,6 +3,8 @@ package com.tekwill.learning.datatypes.operators;
 import java.util.Scanner;
 
 public class ConvertHeight {
+    public static final double ARC_TO_MINUTE = 60.0;
+    public static final double ARC_TO_SECONDS = 3600.0;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your height in integer inches");
--- src/com/tekwill/learning/datatypes/operators/LatitudeDecimalFormat.java
+++ src/com/tekwill/learning/datatypes/operators/LatitudeDecimalFormat.java
@@ -3,6 +3,8 @@ package com.tekwill.learning.datatypes.operators;
 import java.util.Scanner;
 
 public class LatitudeDecimalFormat {
+    public static final double MINUTES_ARC = 60.0;
+    public static final double SECONDS_ARC = 3600.0;
     public  static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, please enter the latitude in degrees, minutes and seconds");
@@ -12,10 +14,9 @@ public class LatitudeDecimalFormat {
         int minutesOfArc = scanner.nextInt();
         System.out.println("Please, enter the seconds of the arc");
         int secondsOfArc = scanner.nextInt();
-        double minutesInDecimal = degrees * 100 / 60;
-        double secondsInDecimal = secondsOfArc * 100 / 60;
-        double degreesInDecimal = degrees + minutesInDecimal * 0.01 + secondsInDecimal * 0.0001;
-        System.out.println("Latitude is " + degreesInDecimal);
+
+        double degreesInDecimal = degrees + (minutesOfArc / MINUTES_ARC) + (secondsOfArc / SECONDS_ARC);
+        System.out.printf("Latitude is %.4f", degreesInDecimal);
 
 
         
