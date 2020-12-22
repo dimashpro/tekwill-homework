9aa529d9b286dac0bfad3b66da810ffd7e0ebaa9
dimashpro <sergiu.proca@gmail.com>
2020-12-22 19:14:52 +0100

    modify HeightConverter

--- src/com/tekwill/learning/datatypes/operators/HeightConverter.java
+++ src/com/tekwill/learning/datatypes/operators/HeightConverter.java
@@ -2,7 +2,7 @@ package com.tekwill.learning.datatypes.operators;
 
 import java.util.Scanner;
 
-public class ConvertHeight {
+public class HeightConverter {
     public static final int INCHES_IN_ONE_FOOT = 12;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
