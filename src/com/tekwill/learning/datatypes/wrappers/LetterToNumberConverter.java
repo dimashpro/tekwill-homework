ad94884f8f4877f288f528f5b82a3bfe3ac14d85
dimashpro <sergiu.proca@gmail.com>
2020-12-27 16:10:43 +0100

    optimized converter from letters to numbers

--- src/com/tekwill/learning/datatypes/wrappers/LetterToNumberConverter.java
+++ src/com/tekwill/learning/datatypes/wrappers/LetterToNumberConverter.java
@@ -7,25 +7,19 @@ public class LetterToNumberConverter {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a letter grade :");
         String letterGrade = scanner.nextLine();
-        Integer numericValue;
         if (letterGrade.equals("A")) {
-            numericValue = 4;
-            System.out.println("The numeric value for grade " + letterGrade + " is " + numericValue);
+            System.out.println("The numeric value for grade " + letterGrade + " is 4");
         } else if (letterGrade.equals("B")) {
-            numericValue = 3;
-            System.out.println("The numeric value for grade " + letterGrade + " is " + numericValue);
+            System.out.println("The numeric value for grade " + letterGrade + " is 3");
         }
         else if (letterGrade.equals("C")) {
-            numericValue = 2;
-            System.out.println("The numeric value for grade " + letterGrade + " is " + numericValue);
+            System.out.println("The numeric value for grade " + letterGrade + " is 2");
         }
         else if (letterGrade.equals("D")){
-            numericValue = 1;
-            System.out.println("The numeric value for grade " + letterGrade + " is " + numericValue);
+            System.out.println("The numeric value for grade " + letterGrade + " is 1");
         }
         else if (letterGrade.equals("E")){
-            numericValue = 0;
-            System.out.println("The numeric value for grade " + letterGrade + " is " + numericValue);
+            System.out.println("The numeric value for grade " + letterGrade + " is 0");
         }
         else {
             System.out.println(letterGrade +" is an invalid grade");
