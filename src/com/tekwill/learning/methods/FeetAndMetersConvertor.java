c35d36e7a7404617ab9764bd791c78b52da62487
dimashpro <sergiu.proca@gmail.com>
2021-01-15 15:52:29 +0100

    refactored code of feet and meters conversion by adding constants

--- src/com/tekwill/learning/methods/FeetAndMetersConvertor.java
+++ src/com/tekwill/learning/methods/FeetAndMetersConvertor.java
@@ -1,10 +1,15 @@
 package com.tekwill.learning.methods;
 
 public class FeetAndMetersConvertor {
-    public static double footToMeter(double foot){
-        return 0.305 * foot;
+
+    public static final double METER_IN_A_FOOT = 0.305;
+    public static final double FOOT_IN_A_METER = 3.279;
+
+    public static double footToMeter(double foot) {
+        return METER_IN_A_FOOT * foot;
     }
-    public static double meterToFoot(double meter){
-        return 3.279 * meter;
+
+    public static double meterToFoot(double meter) {
+        return FOOT_IN_A_METER * meter;
     }
 }
