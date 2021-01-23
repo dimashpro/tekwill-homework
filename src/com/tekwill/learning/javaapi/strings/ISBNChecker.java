c41f9f3c92804c0323339e46edb9b924f3e378ff
dimashpro <sergiu.proca@gmail.com>
2021-01-23 12:49:33 +0100

    fixed logical errors in the ISBNChecker class

--- src/com/tekwill/learning/javaapi/strings/ISBNChecker.java
+++ src/com/tekwill/learning/javaapi/strings/ISBNChecker.java
@@ -4,10 +4,20 @@ public class ISBNChecker {
     public static void printISBN(String isbn) {
 
         char[] chArr = isbn.toCharArray();
-        int checksum = 10 - (chArr[0] + 3 * chArr[1] + chArr[2] + 3 * chArr[3] + chArr[4] + 3 * chArr[5] +
-                chArr[6] + 3 * chArr[7] + chArr[8] + 3 * chArr[9] + chArr[10] + 3 * chArr[11]) % 10;
+        int checksum = 0;
+
+        for (int i = 0; i < isbn.length(); i++) {
+            int intNumber = Integer.parseInt(chArr[i] + "");
+            if ((i + 1) % 2 == 0) {
+                checksum += intNumber;
+            } else {
+                checksum += 3 * intNumber;
+            }
+        }
+        checksum = 10 - checksum % 10;
         if (checksum == 10) {
             checksum = 0;
+
         }
 
         System.out.println("The ISBN-13 number is " + isbn + checksum);
