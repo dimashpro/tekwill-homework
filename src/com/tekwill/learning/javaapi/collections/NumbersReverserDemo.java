1a18210f166cade7ca4194318909146667b2df9d
dimashpro <sergiu.proca@gmail.com>
2021-02-06 14:17:35 +0100

    added NumbersReversedDemo class

--- src/com/tekwill/learning/javaapi/collections/NumbersReverserDemo.java
+++ src/com/tekwill/learning/javaapi/collections/NumbersReverserDemo.java
@@ -1,2 +1,14 @@
-package com.tekwill.learning.javaapi.collections;public class NumbersReverserDemo {
+package com.tekwill.learning.javaapi.collections;
+
+import java.util.Scanner;
+
+public class NumbersReverserDemo {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of the integers that will be read :");
+        int numberOfIntegers = scanner.nextInt();
+        NumbersReverser.reverseNumbers(numberOfIntegers);
+
+    }
 }
