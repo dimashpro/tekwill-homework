555c12b49db625c7b45d75559c656768c697553e
dimashpro <sergiu.proca@gmail.com>
2021-02-08 21:49:21 +0100

    added DigitsSumDemo

--- src/com/tekwill/learning/javaapi/collections/DigitsSumDemo.java
+++ src/com/tekwill/learning/javaapi/collections/DigitsSumDemo.java
@@ -1,2 +1,11 @@
-package com.tekwill.learning.javaapi.collections;public class DigitsSumDemo {
+package com.tekwill.learning.javaapi.collections;
+
+import java.util.Scanner;
+
+public class DigitsSumDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a series of single digit numbers with nothing separating them: ");
+        String value = scanner.nextLine();
+    }
 }
