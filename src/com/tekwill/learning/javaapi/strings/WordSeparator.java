50fa78f65ac149ce782f083e063cc459149168e5
dimashpro <sergiu.proca@gmail.com>
2021-02-11 22:09:46 +0100

    added WordConverter class

--- src/com/tekwill/learning/javaapi/strings/WordSeparator.java
+++ src/com/tekwill/learning/javaapi/strings/WordSeparator.java
@@ -1,2 +1,15 @@
-package com.tekwill.learning.javaapi.strings;public class WordSeparator {
+package com.tekwill.learning.javaapi.strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordSeparator {
+    public static void convertSentence(String sentence) {
+        List<String> newSentence = new ArrayList<>();
+        for (int i = 0; i < sentence.length(); i++) {
+            if (Character.isUpperCase(sentence[i])){
+
+            }
+        }
+    }
 }
