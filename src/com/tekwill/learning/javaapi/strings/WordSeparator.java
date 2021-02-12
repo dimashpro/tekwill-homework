31007f1efc50462223702a72ac273e8579bf59d5
dimashpro <sergiu.proca@gmail.com>
2021-02-12 17:01:39 +0100

    added WordSeparator that convert a sentence

--- src/com/tekwill/learning/javaapi/strings/WordSeparator.java
+++ src/com/tekwill/learning/javaapi/strings/WordSeparator.java
@@ -6,10 +6,18 @@ import java.util.List;
 public class WordSeparator {
     public static void convertSentence(String sentence) {
         List<String> newSentence = new ArrayList<>();
-        for (int i = 0; i < sentence.length(); i++) {
-            if (Character.isUpperCase(sentence[i])){
+        int k = 0;
+        for (int i = 1; i < sentence.length(); i++) {
+
+            if (Character.isUpperCase(sentence.charAt(i)) || (Character.isWhitespace(sentence.charAt(i)))) {
+
+                newSentence.add(sentence.substring(k, i).toLowerCase());
+                k = i;
 
             }
+        }
+        for (String text : newSentence) {
+            System.out.print(text + " ");
         }
     }
 }
