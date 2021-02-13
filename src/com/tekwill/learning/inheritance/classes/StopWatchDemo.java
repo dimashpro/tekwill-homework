a8facd3156cf891bc824f863b46a89cc480fc7f3
dimashpro <sergiu.proca@gmail.com>
2021-02-13 19:34:25 +0100

    added StopWatchDemo

---
+++ src/com/tekwill/learning/inheritance/classes/StopWatchDemo.java
@@ -0,0 +1,8 @@
+package com.tekwill.learning.inheritance.classes;
+
+public class StopWatchDemo {
+    public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.run();
+    }
+}
