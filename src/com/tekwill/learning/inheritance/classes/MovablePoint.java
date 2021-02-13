c2c757ee08980dd7be0bdbcaa037b07fd9f53787
dimashpro <sergiu.proca@gmail.com>
2021-02-13 16:45:50 +0100

    added MovablePoint class that extends Point class and calculate speed of movements of a point

--- src/com/tekwill/learning/inheritance/classes/MovablePoint.java
+++ src/com/tekwill/learning/inheritance/classes/MovablePoint.java
@@ -1,2 +1,60 @@
-package com.tekwill.learning.inheritance.classes;public class MovablePoint {
+package com.tekwill.learning.inheritance.classes;
+
+public class MovablePoint extends Point {
+    private float xSpeed = 0.0f;
+    private float ySpeed = 0.0f;
+
+    public MovablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint() {
+    }
+
+    public float getXSpeed() {
+        return xSpeed;
+    }
+
+    public void setXSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getYSpeed() {
+        return ySpeed;
+    }
+
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public float[] getSpeed() {
+        float[] coordinates = new float[2];
+        coordinates[0] = xSpeed;
+        coordinates[1] = ySpeed;
+        return coordinates;
+    }
+
+    public void setSpeed(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "\"(" + x + "," + y + "),speed=(" + xSpeed + "," + ySpeed + ")\"";
+
+    }
+
+    public MovablePoint move() {
+        x += xSpeed;
+        y += ySpeed;
+        return this;
+    }
 }
