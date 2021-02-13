2bfaf0eaa6b12a52ca73403813cacfbd2f4ad3a3
dimashpro <sergiu.proca@gmail.com>
2021-02-13 12:16:35 +0100

    added StringOperations class

--- src/com/tekwill/learning/javaapi/strings/StringOperations.java
+++ src/com/tekwill/learning/javaapi/strings/StringOperations.java
@@ -1,2 +1,26 @@
-package com.tekwill.learning.javaapi.strings;public class StringOperations {
+package com.tekwill.learning.javaapi.strings;
+
+import java.util.Scanner;
+
+public class StringOperations {
+
+    public static int wordCount(String text){
+        String[] words = text.split(" ");
+        return words.length;
+    }
+
+    public static String arrayToString(char[] charArray){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < charArray.length; i++) {
+            stringBuilder.append(charArray[i]);
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void run(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a sentence");
+        String sentence = scanner.nextLine();
+        System.out.println("In this sentence are " + wordCount(sentence) + " words");
+    }
 }
