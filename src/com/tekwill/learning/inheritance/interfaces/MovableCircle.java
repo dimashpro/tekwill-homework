4c06bb72b3abbfd3ce09be14caca557471f3d59b
dimashpro <sergiu.proca@gmail.com>
2021-02-16 20:59:11 +0100

    added MovableCircle class

--- src/com/tekwill/learning/inheritance/interfaces/MovableCircle.java
+++ src/com/tekwill/learning/inheritance/interfaces/MovableCircle.java
@@ -1,2 +1,57 @@
-package com.tekwill.learning.inheritance.interfaces;public class MovableCircle {
+package com.tekwill.learning.inheritance.interfaces;
+
+import java.util.Objects;
+
+public class MovableCircle implements Movable{
+
+    private int radius;
+    private MovablePoint center;
+
+
+    public MovableCircle(int radius, MovablePoint center) {
+        this.radius = radius;
+        this.center = center;
+    }
+
+    @Override
+    public String toString() {
+        return "MovableCircle{" +
+                "radius=" + radius +
+                ", center=" + center +
+                '}';
+    }
+
+    @Override
+    public void moveUp() {
+        center.y += center.ySpeed;
+    }
+
+    @Override
+    public void moveDown() {
+        center.y -= center.ySpeed;
+    }
+
+    @Override
+    public void moveLeft() {
+        center.x -= center.xSpeed;
+    }
+
+    @Override
+    public void moveRight() {
+        center.x += center.xSpeed;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MovableCircle that = (MovableCircle) o;
+        return radius == that.radius &&
+                Objects.equals(center, that.center);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(radius, center);
+    }
 }
