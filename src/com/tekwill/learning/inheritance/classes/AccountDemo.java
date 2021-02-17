b447518f3e3dbc48c96e71ce9f497820c0d68322
dimashpro <sergiu.proca@gmail.com>
2021-02-17 21:16:37 +0100

    added AccountDemo class that test Account methods

--- src/com/tekwill/learning/inheritance/classes/AccountDemo.java
+++ src/com/tekwill/learning/inheritance/classes/AccountDemo.java
@@ -2,6 +2,10 @@ package com.tekwill.learning.inheritance.classes;
 
 public class AccountDemo {
     public static void main(String[] args) {
-        Account account = new Account();
+        Account account = new Account(1122, 20000);
+        account.setAnnualInterestRate(4.5);
+        account.withdraw(2500);
+        account.deposit(3000);
+        System.out.println(account);
     }
 }
